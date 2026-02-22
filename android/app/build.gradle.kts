dc5c2dab0639be72110d2658451cc762f4f05dc0
dbswhdaks <yunjm671213@gmail.com>
2026-02-22 20:48:24 +0900

    패키지명 변경 com.inovixa.lotto 으로

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.flutter_lotto"
+    namespace = "com.inovixa.lotto"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.flutter_lotto"
+        applicationId = "com.inovixa.lotto"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
